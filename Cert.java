a4223ee3f5416d57b7f9d0fb9e48486f86146bbb
SORIANO Benjamin <benjamin13127@gmail.com>
2016-10-23 21:47:05 +0200

    commit de fin

--- Cert.java
+++ Cert.java
@@ -5,65 +5,69 @@ import java.security.*;
 import java.io.FileWriter;
 
 public class Cert {
-    public static void main(String[] args) {
-		BufferedReader br;
-    String buffe ="";
-		String motLu;
-		String ffinal = "";
-		byte[] buffer, resume;
-		MessageDigest fonction_de_hachage;
-		try {
-			br = new BufferedReader(new FileReader("email1.txt"));
-			while((motLu = br.readLine()) != null)
-			{
-				if(motLu.contains("Message-Id"))
-				{
-          br.readLine();
-          while((motLu = br.readLine()) != null)
-          {
-  					ffinal += motLu;
-  					ffinal += "\r\n";
-          }
-				}
-			}
-      ffinal +="c5dcb78732e1f3966647655229729843\r\n";
-			System.out.println('"'+ffinal+'"');
-		} catch (Exception e) { e.printStackTrace(); }
-		try {
-			buffer = ffinal.getBytes();          // On récupère les octets de la chaîne
-			fonction_de_hachage = MessageDigest.getInstance("MD5");     // On charge MD5
-			resume = fonction_de_hachage.digest(buffer);         // On calcule le résumé
-			System.out.print("Le résumé MD5 de cette chaîne est: ");
-			for(byte octet: resume)
-			System.out.print(String.format("%02X", octet));
-			// On affiche le résumé en hexadécimal
-			System.out.println();
-      try {
-  			br = new BufferedReader(new FileReader("email1.txt"));
-  			while((motLu = br.readLine()) != null)
-  			{
-          buffe += motLu+"\r\n";
-          if(motLu.contains("Message-Id"))
-          {
-            buffe +="X-UdC_authentique: ";
-            for(byte octet: resume)
-              buffe += String.format("%02X", octet);
-            buffe += "\r\n";
-          }
-  			}
-      } catch (Exception e) { e.printStackTrace(); }
-		  } catch (Exception e) { e.printStackTrace(); }
-		try{
-		File f = new File("email1-secure.txt");
-    f.createNewFile();
-		FileWriter fw = new FileWriter(f);
-		fw.write(buffe);
-    fw.close();
-		}catch (Exception e) {e.printStackTrace();}
 
-      /*File f = new File("email1-secure.txt");
-      FileWriter fw = new FileWriter (f);
-      fw.write(buffe);*/
-      System.out.println('"'+buffe+'"');
-    }
+public static void main(String[] args)
+{
+        BufferedReader br;
+        String buffe ="";
+        String motLu;
+        String ffinal = "";
+        byte[] buffer, resume;
+        MessageDigest fonction_de_hachage;
+        try
+        {
+                br = new BufferedReader(new FileReader("email1.txt"));
+                while((motLu = br.readLine()) != null) // Lit email1 Ligne par ligne
+                {
+                        if(motLu.contains("Message-Id")) // A partir de la derniere ligne de l'entete
+                        {
+                                br.readLine(); // saute une ligne ( la ligne vide)
+                                while((motLu = br.readLine()) != null) // Lit le corps du message et l'ajoute a ffinal
+                                {
+                                        ffinal += motLu;
+                                        ffinal += "\r\n";
+                                }
+                        }
+                }
+                ffinal +="c5dcb78732e1f3966647655229729843\r\n"; // ajout du secret
+                System.out.println('"'+ffinal+'"');
+        } catch (Exception e) { e.printStackTrace(); }
+
+        try
+        {
+                buffer = ffinal.getBytes();  // On récupère les octets de la chaîne
+                fonction_de_hachage = MessageDigest.getInstance("MD5"); // On charge MD5
+                resume = fonction_de_hachage.digest(buffer); // On calcule le résumé
+                System.out.print("Le résumé MD5 de cette chaîne est: ");
+                for(byte octet: resume)
+                        System.out.print(String.format("%02X", octet));
+                // On affiche le résumé en hexadécimal
+                System.out.println();
+                try
+                {
+                        br = new BufferedReader(new FileReader("email1.txt"));
+                        while((motLu = br.readLine()) != null) // On lit le fichier email1 ligne par ligne
+                        {
+                                buffe += motLu+"\r\n"; // On ajoute chaque ligne dans buffe
+                                if(motLu.contains("Message-Id")) // A la fin de l'entete on ajoute la ligne X-UdC_authentique avec le résumé
+                                {
+                                        buffe +="X-UdC_authentique: ";
+                                        for(byte octet: resume) // On ajoute le résumé
+                                                buffe += String.format("%02X", octet);
+                                        buffe += "\r\n";
+                                }
+                        }
+                } catch (Exception e) { e.printStackTrace(); }
+        } catch (Exception e) { e.printStackTrace(); }
+        try
+        {
+                File f = new File("email1-secure.txt");
+                f.createNewFile();
+                FileWriter fw = new FileWriter(f); // On ecrit dans email1-secure
+                fw.write(buffe);
+                fw.close();
+        } catch (Exception e) {e.printStackTrace();}
+        System.out.println('"'+buffe+'"');
+}
+
 }
--- Check.java
+++ Check.java
@@ -5,88 +5,93 @@ import java.io.FileWriter;
 
 public class Check {
 
-    private static String trouverMD5()
-    {
-      BufferedReader br;
-  		String motLu;
-  		String ffinal = "";
-  		byte[] buffer, resume;
-      String md5 = "";
-  		MessageDigest fonction_de_hachage;
-  		try {
-  			br = new BufferedReader(new FileReader("email1-secure.txt"));
-  			while((motLu = br.readLine()) != null)
-  			{
-  				if(motLu.contains("X-UdC_authentique:"))
-  				{
-            br.readLine();
-            while((motLu = br.readLine()) != null)
-            {
-    					ffinal += motLu;
-    					ffinal += "\r\n";
-            }
-  				}
-  			}
-        ffinal +="c5dcb78732e1f3966647655229729843\r\n";
-  		} catch (Exception e) { e.printStackTrace(); }
-  		try {
-  			buffer = ffinal.getBytes();          // On récupère les octets de la chaîne
-  			fonction_de_hachage = MessageDigest.getInstance("MD5");     // On charge MD5
-  			resume = fonction_de_hachage.digest(buffer);         // On calcule le résumé
-  			System.out.print("Le résumé MD5 du corps est: ");
-  			for(byte octet: resume)
-  			md5+=String.format("%02X", octet);
-  			// On affiche le résumé en hexadécimal
-  			System.out.println();
-      } catch (Exception e) { e.printStackTrace();}
-      return md5;
-    }
-
-    public static void main(String[] args) {
-		BufferedReader br;
-    String motLu;
-		String appendice = "";
-    char tmp;
-    String md5;
-    boolean cond1 = false;
-		try {
-			br = new BufferedReader(new FileReader("email1-secure.txt"));
-			while((motLu = br.readLine()) != null)
-			{
-				if(motLu.contains("X-UdC_authentique:"))
-				{
-          int i;
-          for(i=0;i<motLu.length();i++)
+          /*
+          /* Trouve le Md5 du corps c de email1-secure
+          */
+          private static String trouverMD5()
           {
-            if(motLu.charAt(i) == ':')
-            {
-              i++;
-              while(motLu.charAt(i) == ' ')
-                i++;
-              while(i<motLu.length())
-              {
-                appendice+=motLu.charAt(i);
-                i++;
-              }
-            }
+                  BufferedReader br;
+                  String motLu;
+                  String ffinal = "";
+                  byte[] buffer, resume;
+                  String md5 = "";
+                  MessageDigest fonction_de_hachage;
+                  try {
+                          br = new BufferedReader(new FileReader("email1-secure.txt"));
+                          while((motLu = br.readLine()) != null)     // Lit email1 ligne par ligne
+                          {
+                                  if(motLu.contains("X-UdC_authentique:"))     // Dernière ligne de l'entête
+                                  {
+                                          br.readLine();
+                                          while((motLu = br.readLine()) != null)     // Lit tout le corps du message
+                                          {
+                                                  ffinal += motLu;
+                                                  ffinal += "\r\n";
+                                          }
+                                  }
+                          }
+                          ffinal +="c5dcb78732e1f3966647655229729843\r\n";     // Ajoute le secret
+                  } catch (Exception e) { e.printStackTrace(); }
+                  try {
+                          buffer = ffinal.getBytes();      // On récupère les octets de la chaîne
+                          fonction_de_hachage = MessageDigest.getInstance("MD5");     // On charge MD5
+                          resume = fonction_de_hachage.digest(buffer);     // On calcule le résumé
+                          System.out.print("Le résumé MD5 du corps est: ");
+                          for(byte octet: resume)
+                                  md5+=String.format("%02X", octet);
+                          // On affiche le résumé en hexadécimal
+                          System.out.println();
+                  } catch (Exception e) { e.printStackTrace();}
+                  return md5;     //retourne le md5
           }
-          cond1 = true;
-				}
-			}
-      if(cond1 == false)
-        System.out.println("Ne contient pas le champ X-UdC_authentique");
-      else if(appendice.length() == 0)
-        System.out.println("Ne contient pas d'appendice");
-      else
-        System.out.println("Appendice : \""+appendice+"\"");
-        md5 = trouverMD5();
-      System.out.println(md5);
-      if(md5.equals(appendice))
-        System.out.println("Bravo ! l'email est authentique !!!");
-      else
-        System.out.println("Attention ! email frauduleux ");
-    } catch(Exception e) {e.printStackTrace();}
-  }
+
+          public static void main(String[] args)
+          {
+                  BufferedReader br;
+                  String motLu;
+                  String appendice = "";
+                  char tmp;
+                  String md5;
+                  boolean cond1 = false;
+                  try
+                  {
+                          br = new BufferedReader(new FileReader("email1-secure.txt"));
+                          while((motLu = br.readLine()) != null)     // Lit l'email1-secure ligne par ligne
+                          {
+                                  if(motLu.contains("X-UdC_authentique:"))     // On identifie la ligne contenant l'appendice h
+                                  {
+                                          int i;
+                                          for(i=0; i<motLu.length(); i++)
+                                          {
+                                                  if(motLu.charAt(i) == ':')     // On récupère l'appendice charactère par charactère
+                                                  {
+                                                          i++;
+                                                          while(motLu.charAt(i) == ' ')
+                                                                  i++;
+                                                          while(i<motLu.length())
+                                                          {
+                                                                  appendice+=motLu.charAt(i);
+                                                                  i++;
+                                                          }
+                                                  }
+                                          }
+                                          cond1 = true;
+                                  }
+                          }
+                          if(cond1 == false)
+                                  System.out.println("Ne contient pas le champ X-UdC_authentique");
+                          else if(appendice.length() == 0)
+                                  System.out.println("Ne contient pas d'appendice");
+                          else
+                                  System.out.println("Appendice : \""+appendice+"\"");
+                          md5 = trouverMD5();
+                          System.out.println(md5);
+                          if(md5.equals(appendice))     // Compare l'appendice h au résumé md5 du corps du message avec le secret
+                                  System.out.println("Bravo ! l'email est authentique !!!");
+                          else
+                                  System.out.println("Attention ! email frauduleux ");
+                  } catch(Exception e) {e.printStackTrace();}
+          }
 
 
 }
